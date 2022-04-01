8aaddcf5e60064c9201e6b94140a066d603711e3
Rael Calitro <rael06@hotmail.fr>
2022-04-01 20:11:21 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import React from "react";
+import classes from "./App.module.scss";
+
+type Weather = {
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+};
 
 function App() {
+  const [weathers, setWeathers] = React.useState<Weather[]>([]);
+
+  async function handleClick() {
+    console.log("hello world");
+    const weatherForecastResponse = await fetch(
+      "http://localhost:7999/WeatherForecast"
+    );
+    setWeathers(await weatherForecastResponse.json());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <div className={classes.headerSection}>
+        <Button color="primary" variant="outlined" onClick={handleClick}>
+          Weatherforecast
+        </Button>
+      </div>
+
+      <div className={classes.bodySection}>
+        {weathers.map((weather) => (
+          <div
+            key={`${weather.summary}${weather.temperatureC}${weather.temperatureF}`}
+            className={classes.weather}
+          >
+            <p>{weather.summary}</p>
+            <p>{weather.temperatureC}</p>
+            <p>{weather.temperatureF}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
